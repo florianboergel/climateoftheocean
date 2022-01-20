f59d76be15d62c7dd46f898f8b2dd33a2fff2147
florianboergel <florianboergel@io-warnemuende.de>
2022-01-20 23:39:08 +0100

    update homework

--- _build/jupyter_execute/2022-01-20-climate-of-the-ocean-h3.py
+++ _build/jupyter_execute/2022-01-20-climate-of-the-ocean-h3.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# # Exercise 3, analyzing the Baltic Sea
+
 # In[29]:
 
 
@@ -20,9 +22,9 @@ ds = xr.open_dataset("data/ocean_day3d.nc")
 # 
 # 
 
-# Look at this figure and zoom on the connection between the world ocean and the Baltic Sea.
+# Look at this figure([Meier and Kauker, 2003](https://agupubs.onlinelibrary.wiley.com/cms/asset/144f12b6-8897-474b-b4e7-4dc1ad3c8faa/)) and zoom on the connection between the world ocean and the Baltic Sea.
 # 
-# (lon=8-17, lat=53-59)
+# The coordinates are : `lon=8-17, lat=53-59`
 # 
 # ![](https://agupubs.onlinelibrary.wiley.com/cms/asset/144f12b6-8897-474b-b4e7-4dc1ad3c8faa/jgrc9306-fig-0001.png)
 
@@ -48,7 +50,7 @@ ds.dims
 # In[72]:
 
 
-danish_straits = ds.sel(xt_ocean=slice(8, 17), yt_ocean=slice(53, 59))
+danish_straits = ds.sel(xt_ocean=slice(,), yt_ocean=slice(,))
 
 
 # In[12]:
@@ -66,9 +68,13 @@ danish_straits.salt.isel(st_ocean=0, time = 0).plot()
 # 
 # Answer: 
 
-# Often this is followed by something similar to:
+# **Question 2:** Look at the colobar. What role play the Danish Straits for the salinity of the Baltic Sea?
 # 
-# >The inflow of freshwater by river runoff, a positive net precipitation cause a positive water balance with respect to the North Sea. The positive water balance leads to strong gradients in salinity and ecosystem variables (Reckermann et al., 2008).
+# Answer:
+
+# Following on common Baltic Sea introductions, you will find something similar to:
+# 
+# > The inflow of freshwater by river runoff and a positive net precipitation cause a positive water balance with respect to the North Sea. The positive water balance leads to strong gradients in salinity and ecosystem variables (Reckermann et al., 2008).
 
 # So let's look at the mean surface salinity!
 
@@ -86,9 +92,9 @@ ds.salt.isel(st_ocean=0).mean("time").plot(levels=level, cmap=plt.cm.jet)
 # 
 # > In the long-term mean, high-saline water from the Kattegat enters the Baltic proper and low-saline water leaves the Baltic because of the freshwater surplus.
 # 
-# think about the exchange flow of the Baltic Sea. How would the profile of a transect a 16°E look like? 
+# Think about the exchange flow of the Baltic Sea. How would the profile of a transect a 16°E look like? 
 
-# In[94]:
+# In[129]:
 
 
 # We first select the latitude range 53-57, then longitude at 16°E
@@ -113,7 +119,7 @@ transect.salt.dims
 # .mean("time")
 # ```
 
-# In[96]:
+# In[130]:
 
 
 transect_mean_time = transect.mean("time")
@@ -121,13 +127,13 @@ transect_mean_time = transect.mean("time")
 
 # Now we can average over the latitude, to give us a depth profile.
 
-# In[97]:
+# In[131]:
 
 
 transect_mean_time_latitude = transect_mean_time.mean("yt_ocean")
 
 
-# In[98]:
+# In[132]:
 
 
 f, ax = plt.subplots(1)
@@ -141,7 +147,7 @@ ax.invert_yaxis()
 # 
 # > The bottom water in the deep subbasins is ventilated mainly by large perturbations, so-called major Baltic saltwater inflows [Matthäus and Franck, 1992; Fischer and Matthäus, 1996]. 
 
-# In this year we have no strong inflow however, we can notice inflows of high saline water analyzing the station located in the Arkona Basin.
+# In this year we have no strong inflow. However, we can notice inflows of high saline water analyzing the station located in the Arkona Basin.
 
 # In[100]:
 
@@ -162,27 +168,27 @@ for ax in g.axes[0]:
 g.fig.tight_layout()
 
 
+# **Question:** Focus on the month of February. What is happening?
+
 # These saline inflows are important for the oxygen supply of the deeper layers, since due to the strong stratification in the Baltic Sea only layers above the permanent halocline are directly influenced by the atmosphere and therefore supplied with oxygen (Mohrholz et al., 2015).
 
 # # Seasonal cycle of the temperature
 
-# In[125]:
+# We will now look at the depth-averaged seasonal cycle of the Baltic Sea.
+
+# In[136]:
 
 
-ds_temp_season = ds.temp.resample(time="1M").mean().isel(st_ocean = 0)
+ds_temp_season = ds.temp.resample(time="1M").mean("time").mean("st_ocean")
 
 
-# In[127]:
+# In[137]:
 
 
-(ds_temp_season-ds_temp_season.mean("time")).plot(col="time", col_wrap =3)
+(ds_temp_season-ds_temp_season.mean()).plot(col="time", col_wrap =3)
 
 
-# In[124]:
-
-
-ds_mean_salt.plot()
-
+# **Question: ** Above you see the deviation from the mean temperature of the Baltic Sea for every single month. Try to discuss the differences for every month.
 
 # ## Sea Ice
 # 
